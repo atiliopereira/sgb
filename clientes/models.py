c9dc2d167d4943530fd1587d0095446ceb70357a
Atilio Pereira <atiliopereira@outlook.com>
2025-09-08 15:28:04 -0300

    Add numero de liquidacion to Cliente model

--- clientes/models.py
+++ clientes/models.py
@@ -4,7 +4,15 @@ from django.db import models
 class Cliente(models.Model):
     nombre = models.CharField(max_length=255)
     ruc = models.CharField(max_length=20)
-    email = models.EmailField()
+    email = models.EmailField(
+        blank=True,
+        null=True,
+    )
+    numero_liquidacion = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
         return self.nombre
