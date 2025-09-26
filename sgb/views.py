928b480485a3e3437636d08b2e095aef3fe27096
Atilio Pereira <atiliopereira@outlook.com>
2025-09-26 01:33:32 -0300

    Add login required for dashboard

--- sgb/views.py
+++ sgb/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from clientes.models import Cliente
 from items.models import Item
 from liquidaciones.models import Liquidacion, Procedencia, Proveedor, Banco, Pago
 
 
+@login_required
 def dashboard(request):
     context = {
         'stats': {
